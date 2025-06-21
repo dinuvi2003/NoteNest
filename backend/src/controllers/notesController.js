9f470462f48dee317a0bc64eafac636ab67b1ea0
dinuvi2003 <dinuvi2003@gmail.com>
2025-06-21 11:13:09 +0530

    first commit

---
+++ backend/src/controllers/notesController.js
@@ -0,0 +1,39 @@
+import Note from "../models/Note.js"
+
+export async function getAllNotes (req,res)  {
+    try {
+        const notes = await Note.find()
+        res.status(200).json(notes)
+    } 
+    catch(error) {
+        console.error("Error in getAllNotes controller", error)
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export async function createNote (req,res)  {
+    try{
+        const {title, content} = req.body
+        const newNote = new Note({title,content})
+        const savedNote = await newNote.save()
+        res.status(201).json(savedNote)
+    } 
+    catch(error) {
+        console.error("Error in createNote controller", error)
+        res.status(500).json({message: "Internal server error"})
+    }
+}
+
+export async function updateNote (req,res)  {
+    try {
+        const {title, content} = req.body
+        await Note.findByIdAndUpdate()
+    }
+    catch(error) {
+
+    }
+}
+
+export function deleteNote (req,res)  {
+    res.status(200).send({message: "Your note has been deleted successfully"})
+}
---
+++ backend/src/server.js
@@ -0,0 +1,24 @@
+import express from "express"
+// const express = require("express")
+import notesRoutes from "./routes/notesRoutes.js"
+import {connectDB} from "./config/db.js"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+console.log(process.env.MONGO_URI)
+const PORT = process.env.PORT || 5001
+
+const app = express()
+
+connectDB()
+
+//middleware
+app.use(express.json())
+
+app.use("/api/notes", notesRoutes)
+
+app.listen(PORT, () => {
+    console.log("Server started on PORT: ", PORT)
+})
+
