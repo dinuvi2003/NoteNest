5995a725f00b41045d3050e78e2a1c0d9806d6a7
dinuvi2003 <dinuvi2003@gmail.com>
2025-07-21 01:42:27 +0530

    prepared to deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,13 @@ import notesRoutes from "./routes/notesRoutes.js"
 import {connectDB} from "./config/db.js"
 import dotenv from "dotenv"
 import cors from "cors"
+import path from "path"
 
 dotenv.config()
 
 console.log(process.env.MONGO_URI)
 const PORT = process.env.PORT || 5001
+const __dirname = path.resolve()
 
 const app = express()
 
@@ -16,10 +18,13 @@ connectDB()
 
 //middleware
 app.use(express.json()) //this middleware will parse JSON bodies: req.body
-app.use(
+
+if(process.env.NODE_ENV !== "production") {
+    app.use(
     cors({
         origin: "http://localhost:5173"
-}))
+    }))
+}
 
 //our simple custom middleware
 app.use((req,res,next) => {
@@ -28,6 +33,14 @@ app.use((req,res,next) => {
 })
 
 app.use("/api/notes", notesRoutes)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*",(res,req) => {
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+})
+}
 
 app.listen(PORT, () => {
     console.log("Server started on PORT: ", PORT)
