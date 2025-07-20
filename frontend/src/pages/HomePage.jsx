ca3e1333729ee35ed7ccdd551bc72935b56a6fe7
dinuvi2003 <dinuvi2003@gmail.com>
2025-07-20 11:07:24 +0530

    Frontend

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Navbar from '../components/Navbar'
+import NoteCard from '../components/NoteCard'
+import NotesNotFound from '../components/NotesNotFound'
 
 const HomePage = () => {
   const [notes,setNotes] = useState([])
@@ -26,10 +28,13 @@ const HomePage = () => {
         <Navbar/>
 
         <div className='max-w-7xl mx-auto p-4 mt-6'>
-          {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
-          {notes.length > 0 && (
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+          {notes.length == 0 && <NotesNotFound/>}
 
+          {notes.length > 0 && (
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+              {notes.map((note) => (
+                <NoteCard key={note._id} note={note} setNotes={setNotes}/>
+              ))}
             </div>
           )}
         </div>
--- frontend/src/pages/NoteDetails.jsx
+++ frontend/src/pages/NoteDetails.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router'
+import toast, { LoaderIcon } from 'react-hot-toast'
 
 const NoteDetails = () => {
+
+  const [note,setNotes] = useState(null);
+  const [loading,setLoading] = useState(true);
+  const [saving,setSaving] = useState(false);
+
+  const navigate = useNavigate()
+
+  const {id} = useParams()
+
+  useEffect(() => {
+    const fetchNote = async() => {
+      try {
+        const res = await api.get(`/notes/${id}`)
+        setNotes(res.data)
+      } catch(error) {
+        console.log("Error in fetching note",error)
+        toast.error("Failed to fetch the note")
+      }
+
+    }
+    fetchNote();
+  }, {id});
+
+  const handleDelete = () => {
+
+  }
+
+  if(loading) {
+    return(
+      <div className='min-h-screen bg-base-200 flex items-center justify-center'>
+        <LoaderIcon className='animate-spin size-10'/>
+      </div>
+    )
+  }
+
   return (
-    <div>NoteDetails</div>
+    <div className='min-h-screen bg-base-200'>
+      <div className='container mx-auto px-4 py-8'>
+        <div className='max-w-2xl mx-auto'>
+        <div className='flex items-center justify-between mb-6'>
+          <Link to="/" className='btn btn-ghost'>
+            <ArrowLeftIcon className='h-5 w-5'/>
+            Back to Notes
+          </Link>
+          <button onClick={handleDelete} className='btn btn-error btn-outline'>
+            <Trash2Icon className='h-5 w-5'/>
+            Delete Note
+          </button>
+        </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
